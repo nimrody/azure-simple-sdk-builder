1f0841d41a5ce8e6fb0b662f2e4f5e6a236af447
Nimrod Mesika <nmesika@cisco.com>
2025-08-09 20:18:27 +0300

    start over

--- app/src/main/java/com/azure/simpleSDK/generator/parser/Parameter.java
+++
@@ -1,9 +0,0 @@
-package com.azure.simpleSDK.generator.parser;
-
-public record Parameter(
-    String name,
-    String in,
-    boolean required,
-    String type,
-    String description
-) {}
--- app/src/main/java/com/azure/simpleSDK/generator/parser/Response.java
+++
@@ -1,7 +0,0 @@
-package com.azure.simpleSDK.generator.parser;
-
-public record Response(
-    String statusCode,
-    String description,
-    String schemaType
-) {}
